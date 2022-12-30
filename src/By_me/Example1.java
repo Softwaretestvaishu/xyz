966388ce729057e305d4cb2487a7abec6e2197de
HP <HP@DESKTOP-7AA621C>
2022-12-30 20:38:17 +0530

    [vaishali]

---
+++ src/By_me/Example1.java
@@ -0,0 +1,24 @@
+package By_me;
+
+public class Example1 {
+	
+	public static void main(String[] args) {
+		
+		
+		//*
+		//**
+		//***
+		//****
+		//*****
+		
+		for(int i=1; i<=5; i++)     //row
+		{
+			for(int j=1; j<=i; j++)    //column
+			{
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+	}
+
+}
---
+++ src/By_me/Example4.java
@@ -0,0 +1,30 @@
+package By_me;
+
+	public class Example4 {
+		
+		public static void main(String[] args) {
+					
+					for(int i=1; i<=5; i++)     //row
+					{
+						for(int j=1; j<i; j++)    //space
+						{
+							System.out.print(" ");
+						}
+						for(int k=i; k<=5; k++)    //column
+						{
+							System.out.print("*");
+						}
+						System.out.println(" ");
+					}
+				}
+
+			}
+
+
+
+
+		
+		
+
+
+
